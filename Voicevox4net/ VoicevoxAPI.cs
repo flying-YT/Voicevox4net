8b27bf41b2c4dafa9abee02accc9e521ba3eca06
root <root@localhost.localdomain>
2023-12-02 01:37:45 -0500

    first comit

---
+++ Voicevox4net/ VoicevoxAPI.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+
+namespace Voicevox4net;
+public class  VoicevoxAPI
+{
+    private readonly string ipPort;
+    private string DownloadPath { set; get; };
+
+    public VoicevoxAPI(string _ipAdress, string _port)
+    {
+        ipPort = "http://" + _ipAdress + ":" + _port;
+        DownloadPath = Directory.GetCurrentDirectory();
+    }
+
+    private async Task<string> AudioQuery(string _text, int _speakerId)
+    {
+        string jsonQuery;
+        using (var httpClient = new HttpClient())
+        {
+            string url = ipPort + "/audio_query?text=" + _text + "&speaker=" + _speakerId;
+            using (var request = new HttpRequestMessage(new HttpMethod("POST"), url))
+            {
+                request.Headers.TryAddWithoutValidation("accept", "application/json");
+
+                request.Content = new StringContent("");
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+
+                var response = await httpClient.SendAsync(request);
+                jsonQuery = await response.Content.ReadAsStringAsync();
+            }
+        }
+        return jsonQuery;
+    }
+
+    public async Task Synthesis(string _title, string _text, bool _upspeak, int _speakerId)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            string url = ipPort + "/synthesis?speaker=" + _speakerId + "&enable_interrogative_upspeak=" + _upspeak;
+            using (var request = new HttpRequestMessage(new HttpMethod("POST"), url))
+            {
+                request.Headers.TryAddWithoutValidation("accept", "audio/wav");
+
+                request.Content = new StringContent(await AudioQuery(_text, _speakerId));
+
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+                var response = await httpClient.SendAsync(request);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    string fileName = _title + ".wav";
+                    using (var fileStream = File.Create(fileName))
+                    {
+                        using (var httpStream = await response.Content.ReadAsStreamAsync())
+                        {
+                            httpStream.CopyTo(fileStream);
+                            fileStream.Flush();
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+        }
+    }
+}
